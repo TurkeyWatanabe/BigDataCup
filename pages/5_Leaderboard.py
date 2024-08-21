55da3dbd1dc65459b3834b984f6c74888289972f
TurkeyWatanabe <77784601+TurkeyWatanabe@users.noreply.github.com>
2024-08-22 00:43:17 +0800

    Update 5_Leaderboard.py

--- pages/5_Leaderboard.py
+++ pages/5_Leaderboard.py
@@ -28,7 +28,7 @@ st.markdown("<center>Washington DC, USA</center>", unsafe_allow_html=True)
 
 st.markdown("### <span style='color:#1E1C63'>Challenges of Trustworthy AI in Distribution Shifts and Algorithmic Fairness 2024</span>", unsafe_allow_html=True)
 
-engine = create_engine('sqlite:///mount/src/bigdatacup/bigdatacup.db')
+engine = create_engine('sqlite:////mount/src/bigdatacup/bigdatacup.db')
 
 # conn = st.connection('bigdatacupdb', type='sql')
 
