ed6ad4e85944bf4a7fe7f63cdd616279ae63c2b2
TurkeyWatanabe <77784601+TurkeyWatanabe@users.noreply.github.com>
2024-09-03 17:51:04 +0000

    update without filestream

--- pages/3_Submissions.py
+++ pages/3_Submissions.py
@@ -30,15 +30,26 @@ st.markdown("### <span style='color:#1E1C63'>Challenges of Trustworthy AI in Dis
 
 st.markdown("#### Submission Rules")
 
-st.markdown("The submission will be divided into three stages, with specific timing detailed in the <a href='http://192.168.1.112:8501/Important_Dates' target='_self'>Important Dates</a>. Each team is allowed unlimited submissions in each stage. The final result for each stage will be based on the team's last submission, and interim results for each stage will be announced on <a href='http://192.168.1.112:8501/Leaderboard' target='_self'>Leaderboard</a> within one week after the deadline. The team's final score will be based on the results of the third stage."
+st.markdown("The submission will be divided into two stages, with specific timing detailed in the <a href='http://192.168.1.112:8501/Important_Dates' target='_self'>Important Dates</a>. Each team is allowed unlimited submissions in each stage. The final result for each stage will be based on the team's last submission, and interim results for each stage will be announced on <a href='http://192.168.1.112:8501/Leaderboard' target='_self'>Leaderboard</a> within one week after the deadline. The team's final score will be based on the results of the third stage."
             , unsafe_allow_html=True)
 
+# st.markdown("""
+#             Each submission should be a `.zip` file named **only** by the team id (such as `Ix9oW1.zip`), which contains two folders, one for the runnable source code of task1 and the other for task2 (if necessary, include `requirement.txt`), and both should include a function `evaluation` for testing with the following input and output formats:
+#             + Input: 10000 three channel facial images of different sizes from Sketch domain.
+#             + Output: The metric result obtained from trained classifiers (Accuracy for task 1; Accuracy and $\Delta{DP}$ for task 2).
+
+#             At the end of each stage, we will test the team's last submission on our **test set** and publish the results on <a href='http://192.168.1.112:8501/Leaderboard' target='_self'>Leaderboard</a> within a week.
+            
+#             **Note:** incorrectly named files will not be received!!!
+#             """
+#             , unsafe_allow_html=True)
+
 st.markdown("""
-            Each submission should be a `.zip` file named **only** by the team id (such as `Ix9oW1.zip`), which contains two folders, one for the runnable source code of task1 and the other for task2 (if necessary, include `requirement.txt`), and both should include a function `evaluation` for testing with the following input and output formats:
+            Each submission should be a `.zip` file named **only** by the **team leader's email** (such as `xxx@gmail.com.zip`), which contains two folders, one for the runnable source code of task1 and the other for task2 (if necessary, include `requirement.txt`), and both should include a function `evaluation` for testing with the following input and output formats:
             + Input: 10000 three channel facial images of different sizes from Sketch domain.
             + Output: The metric result obtained from trained classifiers (Accuracy for task 1; Accuracy and $\Delta{DP}$ for task 2).
 
-            At the end of each stage, we will test the team's last submission on our **test set** and publish the results on <a href='http://192.168.1.112:8501/Leaderboard' target='_self'>Leaderboard</a> within a week.
+            At the end of each stage, we will test the team's last submission on our **test set** and publish the results on *Leaderboard* within a week.
             
             **Note:** incorrectly named files will not be received!!!
             """
@@ -48,35 +59,39 @@ st.markdown("#### Submission channel")
 
 stage = 1
 st.warning(f"Currently in the stage {stage} of submission")
-st.info(f"Each submission should be a `.zip` file named **only** by the team id (such as `Ix9oW1.zip`)")
-uploaded_file = st.file_uploader("Submit a `.zip` file named only after the team ID.", type="zip")
-if st.button("Submit"):
-    if uploaded_file is not None:
-        # 检查命名格式
-        if uploaded_file.name.endswith(".zip"):
-            team_id = uploaded_file.name[:-4]
-            # 检查是不是合法的team id格式
-            if is_valid_team_id(team_id):
-                # 检查team id是否存在
-                # conn = st.connection('bigdatacupdb', type='sql')
-                engine = create_engine('sqlite:////mount/src/bigdatacup/bigdatacup.db')
-                with engine.connect() as connection:
-                    # team_id_df = conn.query(f"select team_id from teamsForCup;",ttl=2)
-                    team_id_df = connection.execute(text(f"select team_id from teamsForCup;"))
-                    team_id_df = pd.DataFrame(team_id_df.fetchall(), columns=team_id_df.keys())
+st.info(f"Each submission should be a `.zip` file named **only** by the **team leader's email** (such as `xxx@gmail.com.zip`)")
+
+st.markdown("Please use the following link to submit the file:")
+st.markdown("https://docs.google.com/forms/d/e/1FAIpQLSftB0ZA_rFMuVLkrM8HWRPyjGauXiCtmHfwHBf5dw5E27FfIw/viewform?usp=sf_link")
+
+# uploaded_file = st.file_uploader("Submit a `.zip` file named only after the team ID.", type="zip")
+# if st.button("Submit"):
+#     if uploaded_file is not None:
+#         # 检查命名格式
+#         if uploaded_file.name.endswith(".zip"):
+#             team_id = uploaded_file.name[:-4]
+#             # 检查是不是合法的team id格式
+#             if is_valid_team_id(team_id):
+#                 # 检查team id是否存在
+#                 # conn = st.connection('bigdatacupdb', type='sql')
+#                 engine = create_engine('sqlite:////mount/src/bigdatacup/bigdatacup.db')
+#                 with engine.connect() as connection:
+#                     # team_id_df = conn.query(f"select team_id from teamsForCup;",ttl=2)
+#                     team_id_df = connection.execute(text(f"select team_id from teamsForCup;"))
+#                     team_id_df = pd.DataFrame(team_id_df.fetchall(), columns=team_id_df.keys())
                 
-                team_id_list = team_id_df['team_id'].to_list()
-                st.success(team_id_list)
-                if team_id in team_id_list:
-                    with open(os.path.join(f"/mount/src/bigdatacup/uploads/stage{stage}", uploaded_file.name), "wb") as f:
-                        f.write(uploaded_file.getbuffer())
-                    st.success(f'Team {team_id} submitted successfully!', icon="✅")
-                else:
-                    st.error(f'Team {team_id} not registered, please register first.', icon="🚨")
-            else:
-                st.error('Please check the naming format, it can only be team ID.', icon="🚨")
-        else:
-            st.error('Please upload a .zip file.', icon="🚨")
-    else:
-        st.error('Please upload a file first.', icon="🚨")
+#                 team_id_list = team_id_df['team_id'].to_list()
+#                 st.success(team_id_list)
+#                 if team_id in team_id_list:
+#                     with open(os.path.join(f"/mount/src/bigdatacup/uploads/stage{stage}", uploaded_file.name), "wb") as f:
+#                         f.write(uploaded_file.getbuffer())
+#                     st.success(f'Team {team_id} submitted successfully!', icon="✅")
+#                 else:
+#                     st.error(f'Team {team_id} not registered, please register first.', icon="🚨")
+#             else:
+#                 st.error('Please check the naming format, it can only be team ID.', icon="🚨")
+#         else:
+#             st.error('Please upload a .zip file.', icon="🚨")
+#     else:
+#         st.error('Please upload a file first.', icon="🚨")
     
